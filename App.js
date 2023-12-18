bc15d79c9e5590135dd722bd66ee5a5947b9c364
sandalu95 <sandalukalpanee@gmail.com>
2023-12-18 16:57:56 +1100

    basic components and styling

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>Another piece of text!</Text>
+      </View>
+      <Text style={styles.dummyText}>Hello World!</Text>
+      <Button title='Tap Me' />
     </View>
   );
 }
@@ -17,4 +19,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: 'blue',
+    padding: 16,
+  }
 });
