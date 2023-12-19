96455032e84c2c16adc05d89f0235e18946e057d
sandalu95 <sandalukalpanee@gmail.com>
2023-12-19 17:52:07 +1100

    goal input custom component

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import { useState } from 'react'
-import { Button, StyleSheet, TextInput, View, FlatList } from 'react-native'
+import { StyleSheet, View, FlatList } from 'react-native'
 import GoalItem from './components/GoalItem'
+import GoalInput from './components/GoalInput'
 
 export default function App () {
-  const [enteredGoalText, setEnteredGoalText] = useState('')
-  const [courseGoals, setCourseGoals] = useState([])
+  const [courseGoals, setCourseGoals] = useState([]);
 
-  function goalInputHandler (enteredText) {
-    setEnteredGoalText(enteredText)
-  }
-  function addGoalHandler () {
+  function addGoalHandler (enteredGoalText) {
     // setCourseGoals([...courseGoals, enteredGoalText]); Not a better way
     setCourseGoals(currentCourseGoals => [
       ...currentCourseGoals,
@@ -23,14 +20,7 @@ export default function App () {
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder='Your course goal!'
-          style={styles.textInput}
-          onChangeText={goalInputHandler}
-        />
-        <Button title='Add goal' onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         {/* //ScrollView is not good for longer lists because all of the items will be rendered at once even if they are not visible
         <ScrollView alwaysBounceVertical={false}>
@@ -61,22 +51,6 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 16,
     flex: 1
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc'
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8
   },
   goalsContainer: {
     flex: 5
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,48 @@
-function GoalInput () {
+import { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
 
-};
+function GoalInput (props) {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
 
-export default GoalInput;
+  function goalInputHandler (enteredText) {
+    setEnteredGoalText(enteredText)
+  }
+
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText('');
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        placeholder='Your course goal!'
+        style={styles.textInput}
+        onChangeText={goalInputHandler}
+        value={enteredGoalText}
+      />
+      <Button title='Add goal' onPress={addGoalHandler} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
+  }
+})
+
+export default GoalInput
