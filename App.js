bd94bc215c585cbf3eaedb0ac49c9a8552469a9e
sandalu95 <sandalukalpanee@gmail.com>
2023-12-19 17:00:38 +1100

    goal list rendering using flat list

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import { useState } from 'react'
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  FlatList
+} from 'react-native'
 
 export default function App () {
   const [enteredGoalText, setEnteredGoalText] = useState('')
@@ -12,7 +20,11 @@ export default function App () {
     // setCourseGoals([...courseGoals, enteredGoalText]); Not a better way
     setCourseGoals(currentCourseGoals => [
       ...currentCourseGoals,
-      enteredGoalText
+      //Can convert the course goals string array to an object array with text and key so that FlatList can access this key of each item
+      // { text: enteredGoalText, key: Math.random().toString() }
+
+      //If the object doesn't have a property called 'key' but has an id like this, the keyExtractor prop should be used in FlatList
+      { text: enteredGoalText, id: Math.random().toString() }
     ])
   }
 
@@ -27,9 +39,29 @@ export default function App () {
         <Button title='Add goal' onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map(goal => (
-          <Text key={goal}>{goal}</Text>
-        ))}
+        {/* //ScrollView is not good for longer lists because all of the items will be rendered at once even if they are not visible
+        <ScrollView alwaysBounceVertical={false}>
+          {courseGoals.map(goal => (
+            <View key={goal} style={styles.goalItem}>
+              <Text style={styles.goalItemText}>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView> */}
+        <FlatList
+          data={courseGoals}
+          alwaysBounceVertical={false}
+          renderItem={itemData => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalItemText}>{itemData.item.text}</Text>
+              </View>
+            )
+          }}
+          //To extract the key from the items if the item object doesn't have a property named key
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        />
       </View>
     </View>
   )
@@ -52,12 +84,21 @@ const styles = StyleSheet.create({
   },
   textInput: {
     borderWidth: 1,
-    borderBlockColor: '#cccccc',
+    borderColor: '#cccccc',
     width: '70%',
     marginRight: 8,
     padding: 8
   },
   goalsContainer: {
     flex: 5
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc'
+  },
+  goalItemText: {
+    color: 'white'
   }
 })
